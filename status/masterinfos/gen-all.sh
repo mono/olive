10818e686446572ff997437df9fc3f2d3020471d
Atsushi Eno <atsushieno@gmail.com>
2006-10-30 03:50:08 +0000

    2006-10-30  Atsushi Enomoto  <atsushi@ximian.com>
    	* deploy/cm/*
    	  deploy/images/* : added missing files for status pages.
    	* masterinfos/gen-all.sh : added status page generator helper script.
    	* create-status-pages.txt : added how to create status pages.
    
    
    svn path=/trunk/olive/; revision=67120

---
+++ status/masterinfos/gen-all.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+../mono-api-info System.Runtime.Serialization.dll > System.Runtime.Serialization.xml
+../mono-api-info System.IdentityModel.dll > System.IdentityModel.xml
+../mono-api-info System.IdentityModel.Selectors.dll > System.IdentityModel.Selectors.xml
+../mono-api-info System.ServiceModel.dll > System.ServiceModel.xml
