227d3e4097988590abc855dc27ed1a771b65f33b
Atsushi Eno <atsushieno@gmail.com>
2007-01-10 10:39:20 +0000

    2007-01-10  Atsushi Enomoto  <atsushi@ximian.com>
    	* samplesvc6.cs, samplecli6.cs, Makefile: added almost identical
    	  case to sample #3 (differ in key derivation and x509 clause).
    
    
    svn path=/trunk/olive/; revision=70763

---
+++ samples/services/secure-message/samplecli6.cs
@@ -0,0 +1,140 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Xml;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Security;
+using System.ServiceModel.Security.Tokens;
+
+public class Tset
+{
+	public static void Main ()
+	{
+		try {
+			Run ();
+		} catch (MessageSecurityException ex) {
+			Console.WriteLine ("MessageSecurityException::::::");
+			FaultException f = ex.InnerException as FaultException;
+			if (f != null) 
+				using (XmlWriter w = XmlWriter.Create (Console.Out)) {
+					f.CreateMessageFault ().WriteTo (w, EnvelopeVersion.Soap12);
+				}
+			else
+				Console.WriteLine (ex);
+		} catch (WebException ex) {
+			Console.WriteLine (ex);
+			using (StreamReader sr = new StreamReader (ex.Response.GetResponseStream ()))
+				Console.WriteLine (sr.ReadToEnd ());
+		}
+	}
+
+	static void Run ()
+	{
+		SymmetricSecurityBindingElement sbe =
+			new SymmetricSecurityBindingElement ();
+		sbe.MessageSecurityVersion = MessageSecurityVersion.WSSecurity11WSTrustFebruary2005WSSecureConversationFebruary2005WSSecurityPolicy11BasicSecurityProfile10;
+		sbe.RequireSignatureConfirmation = true;
+		//sbe.IncludeTimestamp = false;
+
+		X509SecurityTokenParameters p =
+			new X509SecurityTokenParameters (X509KeyIdentifierClauseType.IssuerSerial, SecurityTokenInclusionMode.AlwaysToRecipient);
+		p.RequireDerivedKeys = false;
+		sbe.EndpointSupportingTokenParameters.Endorsing.Add (p);
+		sbe.ProtectionTokenParameters =
+			new X509SecurityTokenParameters (X509KeyIdentifierClauseType.Thumbprint, SecurityTokenInclusionMode.Never);
+		sbe.SetKeyDerivation (false);
+		//sbe.MessageProtectionOrder = MessageProtectionOrder.SignBeforeEncrypt;
+		HttpTransportBindingElement hbe =
+			new HttpTransportBindingElement ();
+		CustomBinding binding = new CustomBinding (new XBE (), sbe, hbe);
+		X509Certificate2 cert = new X509Certificate2 ("test.pfx", "mono");
+		X509Certificate2 cert2 = cert;//new X509Certificate2 ("test2.cer");
+		FooProxy proxy = new FooProxy (binding,
+			new EndpointAddress (new Uri ("http://localhost:8080"), new X509CertificateEndpointIdentity (cert2)));
+		//proxy.ClientCredentials.ServiceCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.None;
+		proxy.ClientCredentials.ClientCertificate.Certificate = cert;
+		proxy.Endpoint.Behaviors.Add (new StdErrInspectionBehavior ());
+		proxy.Open ();
+		Console.WriteLine (proxy.Echo ("TEST FOR ECHO"));
+	}
+
+	class XBE : BindingElement
+	{
+		public override bool CanBuildChannelFactory<T> (BindingContext ctx)
+		{
+			return ctx.CanBuildInnerChannelFactory<T> ();
+		}
+
+		public override bool CanBuildChannelListener<T> (BindingContext ctx)
+		{
+			return ctx.CanBuildInnerChannelListener<T> ();
+		}
+
+		void DumpParts (ScopedMessagePartSpecification smp)
+		{
+			foreach (string name in smp.Actions) {
+				MessagePartSpecification mp;
+				smp.TryGetParts (name, out mp);
+				Console.WriteLine ("{0}: {1}", name, mp.IsBodyIncluded);
+				foreach (XmlQualifiedName qn in mp.HeaderTypes)
+					Console.WriteLine (qn);
+			}
+			Console.WriteLine ("ChannelParts: {0}", smp.ChannelParts.IsBodyIncluded);
+			foreach (XmlQualifiedName qn in smp.ChannelParts.HeaderTypes)
+				Console.WriteLine (qn);
+		}
+
+		public override IChannelFactory<T> BuildChannelFactory<T> (BindingContext ctx)
+		{
+			ChannelProtectionRequirements cp = ctx.BindingParameters.Find<ChannelProtectionRequirements> ();
+			DumpParts (cp.IncomingEncryptionParts);
+			DumpParts (cp.IncomingSignatureParts);
+			DumpParts (cp.OutgoingEncryptionParts);
+			DumpParts (cp.OutgoingSignatureParts);
+			return ctx.BuildInnerChannelFactory<T> ();
+		}
+
+		public override IChannelListener<T> BuildChannelListener<T> (BindingContext ctx)
+		{
+			return ctx.BuildInnerChannelListener<T> ();
+		}
+
+		public override BindingElement Clone ()
+		{
+			return this;
+		}
+
+		public override T GetProperty<T> (BindingContext ctx)
+		{
+			return ctx.GetInnerProperty<T> ();
+		}
+	}
+}
+
+//[ServiceContract (ProtectionLevel = ProtectionLevel.None)]// (SessionMode = SessionMode.NotAllowed)]
+[ServiceContract]
+public interface IFoo
+{
+	[OperationContract]
+	string Echo (string msg);
+}
+
+public class FooProxy : ClientBase<IFoo>, IFoo
+{
+	public FooProxy (Binding binding, EndpointAddress address)
+		: base (binding, address)
+	{
+	}
+
+	public string Echo (string msg)
+	{
+		return Channel.Echo (msg);
+	}
+}
+
---
+++ samples/services/secure-message/samplesvc6.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Security;
+using System.ServiceModel.Security.Tokens;
+
+	public class GodUserNamePasswordValidator : UserNamePasswordValidator
+	{
+		public override void Validate (string userName, string password)
+		{
+			// God allows everyone.
+		}
+	}
+
+	public class AllowAllX509CertificateValidator : X509CertificateValidator
+	{
+		public override void Validate(X509Certificate2 certificate)
+		{
+		}
+	}
+
+public class Test
+{
+	public static void Main ()
+	{
+		SymmetricSecurityBindingElement sbe =
+			new SymmetricSecurityBindingElement ();
+		sbe.MessageSecurityVersion = MessageSecurityVersion.WSSecurity11WSTrustFebruary2005WSSecureConversationFebruary2005WSSecurityPolicy11BasicSecurityProfile10;
+		sbe.RequireSignatureConfirmation = true;
+		//sbe.IncludeTimestamp = false;
+
+		sbe.ProtectionTokenParameters =
+			new X509SecurityTokenParameters (X509KeyIdentifierClauseType.Thumbprint, SecurityTokenInclusionMode.Never);
+		X509SecurityTokenParameters p =
+			new X509SecurityTokenParameters (X509KeyIdentifierClauseType.IssuerSerial, SecurityTokenInclusionMode.AlwaysToRecipient);
+		p.RequireDerivedKeys = false;
+		sbe.EndpointSupportingTokenParameters.Endorsing.Add (p);
+		sbe.SetKeyDerivation (false);
+		//sbe.MessageProtectionOrder = MessageProtectionOrder.SignBeforeEncrypt;
+		ServiceHost host = new ServiceHost (typeof (Foo));
+		HttpTransportBindingElement hbe =
+			new HttpTransportBindingElement ();
+		CustomBinding binding = new CustomBinding (sbe, hbe);
+		binding.ReceiveTimeout = TimeSpan.FromSeconds (5);
+		host.AddServiceEndpoint ("IFoo",
+			binding, new Uri ("http://localhost:8080"));
+		ServiceCredentials cred = new ServiceCredentials ();
+		cred.ServiceCertificate.Certificate =
+			new X509Certificate2 ("test.pfx", "mono");
+		cred.ClientCertificate.Authentication.CertificateValidationMode =
+			X509CertificateValidationMode.None;
+		host.Description.Behaviors.Add (cred);
+		host.Description.Behaviors.Find<ServiceDebugBehavior> ()
+			.IncludeExceptionDetailInFaults = true;
+		foreach (ServiceEndpoint se in host.Description.Endpoints)
+			se.Behaviors.Add (new StdErrInspectionBehavior ());
+		ServiceMetadataBehavior smb = new ServiceMetadataBehavior ();
+		smb.HttpGetEnabled = true;
+		smb.HttpGetUrl = new Uri ("http://localhost:8080/wsdl");
+		host.Description.Behaviors.Add (smb);
+		host.Open ();
+		Console.WriteLine ("Hit [CR] key to close ...");
+		Console.ReadLine ();
+		host.Close ();
+	}
+}
+
+[ServiceContract]// (SessionMode = SessionMode.NotAllowed)]
+public interface IFoo
+{
+	[OperationContract]
+	string Echo (string msg);
+}
+
+[ServiceBehavior (IncludeExceptionDetailInFaults = true)]
+class Foo : IFoo
+{
+	public string Echo (string msg) 
+	{
+Console.WriteLine (msg);
+		return msg + msg;
+		//throw new NotImplementedException ();
+	}
+}
+
