9b8c3bc0f7911856f8740f06da1b1d974c44631f
Atsushi Eno <atsushieno@gmail.com>
2007-02-28 07:15:17 +0000

    2007-02-28  Atsushi Enomoto  <atsushi@ximian.com>
    	* XElementSequence.cs, Makefile : build fix for recent gmcs updates,
    	  and some more implementation.
    
    
    svn path=/trunk/olive/; revision=73524

--- class/System.Xml.Linq/System.Xml.Linq/XElementSequence.cs
+++ class/System.Xml.Linq/System.Xml.Linq/XElementSequence.cs
@@ -7,35 +7,46 @@ using System.Runtime.CompilerServices;
 
 namespace System.Xml.Linq
 {
-	[ExtensionAttribute]
-	public static class XElementSequence
+	public static class Extensions
 	{
-		[ExtensionAttribute]
-		public static IEnumerable <XElement> Ancestors (IEnumerable <XElement> source)
+		public static IEnumerable<XElement> Ancestors<T> (this IEnumerable<T> source) where T : XNode
 		{
-			foreach (XElement item in source)
-				foreach (XElement elem in item.Ancestors ())
-					yield return elem;
+			foreach (T item in source)
+				for (XElement n = item.Parent as XElement; n != null; n = n.Parent as XElement)
+					yield return n;
 		}
 
-		[ExtensionAttribute]
-		public static IEnumerable <XElement> Ancestors (IEnumerable <XElement> source, XName name)
+		public static IEnumerable<XElement> Ancestors<T> (this IEnumerable<T> source, XName name) where T : XNode
 		{
-			foreach (XElement item in source)
-				foreach (XElement elem in item.Ancestors (name))
-					yield return elem;
+			foreach (T item in source)
+				for (XElement n = item.Parent as XElement; n != null; n = n.Parent as XElement)
+					if (n.Name == name)
+						yield return n;
 		}
 
-		[ExtensionAttribute]
-		public static IEnumerable <XAttribute> Attributes (IEnumerable <XElement> source)
+		public static IEnumerable<XElement> AncestorsAndSelf (this IEnumerable<XElement> source)
+		{
+			foreach (XElement item in source)
+				for (XElement n = item as XElement; n != null; n = n.Parent as XElement)
+					yield return n;
+		}
+
+		public static IEnumerable<XElement> AncestorsAndSelf (this IEnumerable<XElement> source, XName name)
+		{
+			foreach (XElement item in source)
+				for (XElement n = item as XElement; n != null; n = n.Parent as XElement)
+					if (n.Name == name)
+						yield return n;
+		}
+
+		public static IEnumerable <XAttribute> Attributes (this IEnumerable <XElement> source)
 		{
 			foreach (XElement item in source)
 				foreach (XAttribute attr in item.Attributes ())
 					yield return attr;
 		}
 
-		[ExtensionAttribute]
-		public static IEnumerable <XAttribute> Attributes (IEnumerable <XElement> source, XName name)
+		public static IEnumerable <XAttribute> Attributes (this IEnumerable <XElement> source, XName name)
 		{
 			foreach (XElement item in source)
 				foreach (XAttribute attr in item.Attributes (name))
