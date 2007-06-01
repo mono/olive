4e8dae94830dc1ec5bd7fe225013cba133eda3b8
Sebastien Pouliot <sebastien@ximian.com>
2007-06-01 15:25:37 +0000

    Complete Shape class with the new S.W.Media enums
    svn path=/trunk/olive/; revision=78419

--- class/agclr/System.Windows.Shapes/Shape.cs
+++ class/agclr/System.Windows.Shapes/Shape.cs
@@ -33,9 +33,17 @@ namespace System.Windows.Shapes {
 	public abstract class Shape : FrameworkElement {
 
 		public static readonly DependencyProperty FillProperty = DependencyProperty.Register ("Fill", typeof (Brush), typeof (Shape));
+		public static readonly DependencyProperty StretchProperty = DependencyProperty.Register ("Stretch", typeof (Stretch), typeof (Shape));
 		public static readonly DependencyProperty StrokeProperty = DependencyProperty.Register ("Stroke", typeof (Brush), typeof (Shape));
+		public static readonly DependencyProperty StrokeDashArrayProperty = DependencyProperty.Register ("StrokeDashArray", typeof (double[]), typeof (Shape));
+		public static readonly DependencyProperty StrokeDashCapProperty = DependencyProperty.Register ("StrokeDashCap", typeof (PenLineCap), typeof (Shape));
+		public static readonly DependencyProperty StrokeDashOffsetProperty = DependencyProperty.Register ("StrokeDashOffset", typeof (double), typeof (Shape));
+		public static readonly DependencyProperty StrokeEndLineCapProperty = DependencyProperty.Register ("StrokeEndLineDashCap", typeof (PenLineCap), typeof (Shape));
+		public static readonly DependencyProperty StrokeLineJoinProperty = DependencyProperty.Register ("StrokeLineJoin", typeof (PenLineJoin), typeof (Shape));
+		public static readonly DependencyProperty StrokeMiterLimitProperty = DependencyProperty.Register ("StrokeMiterLimit", typeof (double), typeof (Shape));
+		public static readonly DependencyProperty StrokeStartLineCapProperty = DependencyProperty.Register ("StrokeStartLineDashCap", typeof (PenLineCap), typeof (Shape));
 		public static readonly DependencyProperty StrokeThicknessProperty = DependencyProperty.Register ("StrokeThickness", typeof (double), typeof (Shape));
-
+		
 		public Shape ()
 		{
 		}
@@ -45,9 +53,49 @@ namespace System.Windows.Shapes {
 			set { SetValue (FillProperty, value); }
 		}
 
+		public Stretch Stretch {
+			get { return (Stretch) GetValue (StretchProperty); }
+			set { SetValue (StretchProperty, value); }
+		}
+
 		public Brush Stroke {
 			get { return (Brush) GetValue (StrokeProperty); }
 			set { SetValue (StrokeProperty, value); }
+		}
+
+		public double[] StrokeDashArray {
+			get { return (double[]) GetValue (StrokeDashArrayProperty); }
+			set { SetValue (StrokeDashArrayProperty, value); }
+		}
+
+		public PenLineCap StrokeDashCap {
+			get { return (PenLineCap) GetValue (StrokeDashCapProperty); }
+			set { SetValue (StrokeDashCapProperty, value); }
+		}
+
+		public double StrokeDashOffset {
+			get { return (double) GetValue (StrokeDashOffsetProperty); }
+			set { SetValue (StrokeDashOffsetProperty, value); }
+		}
+
+		public PenLineCap StrokeEndLineCap {
+			get { return (PenLineCap) GetValue (StrokeEndLineCapProperty); }
+			set { SetValue (StrokeEndLineCapProperty, value); }
+		}
+
+		public PenLineJoin StrokeLineJoin {
+			get { return (PenLineJoin) GetValue (StrokeLineJoinProperty); }
+			set { SetValue (StrokeLineJoinProperty, value); }
+		}
+
+		public double StrokeMiterLimit {
+			get { return (double) GetValue (StrokeMiterLimitProperty); }
+			set { SetValue (StrokeMiterLimitProperty, value); }
+		}
+
+		public PenLineCap StrokeStartLineCap {
+			get { return (PenLineCap) GetValue (StrokeStartLineCapProperty); }
+			set { SetValue (StrokeStartLineCapProperty, value); }
 		}
 
 		public double StrokeThickness {
