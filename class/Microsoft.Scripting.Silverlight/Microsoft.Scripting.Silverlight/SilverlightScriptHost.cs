9ad4977bb97945bacc1f5d529765812dfd88da0b
Atsushi Eno <atsushieno@gmail.com>
2007-06-07 13:31:11 +0000

    2007-06-06  Atsushi Enomoto  <atsushi@ximian.com>
    	* HtmlElementMemberInjector.cs IDlrNameScope.cs XamlDlrScriptHost.cs
    	  ErrorHandler.cs HtmlDocumentMemberInjector.cs SilverlightPAL.cs :
    	  headers.
    	* SilverlightScriptHost.cs : header. For now directly invoke base.
    
    
    svn path=/trunk/olive/; revision=78838

--- class/Microsoft.Scripting.Silverlight/Microsoft.Scripting.Silverlight/SilverlightScriptHost.cs
+++ class/Microsoft.Scripting.Silverlight/Microsoft.Scripting.Silverlight/SilverlightScriptHost.cs
@@ -1,3 +1,30 @@
+//
+// SilverlightScriptHost.cs
+//
+// Authors:
+//	Atsushi Enomoto  <atsushi@ximian.com>
+//
+// Copyright (C) 2007 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,66 +40,65 @@ namespace Microsoft.Scripting.Silverlight
 		public SilverlightScriptHost (IScriptEnvironment environment)
 			: base (environment)
 		{
-			throw new NotImplementedException ();
 		}
 
 		[MonoTODO]
 		public override void EngineCreated (IScriptEngine engine)
 		{
-			throw new NotImplementedException ();
+			base.EngineCreated (engine);
 		}
 
 		[MonoTODO]
 		public override string [] GetSourceFileNames (string mask, string searchPattern)
 		{
-			throw new NotImplementedException ();
+			return base.GetSourceFileNames (mask, searchPattern);
 		}
 
 		[MonoTODO]
 		public override SourceFileUnit GetSourceFileUnit (IScriptEngine engine, string path, string name)
 		{
-			throw new NotImplementedException ();
+			return base.GetSourceFileUnit (engine, path, name);
 		}
 
 		[MonoTODO]
 		public override void ModuleCreated (IScriptModule module)
 		{
-			throw new NotImplementedException ();
+			base.ModuleCreated (module);
 		}
 
 		[MonoTODO]
 		public override string NormalizePath (string path)
 		{
-			throw new NotImplementedException ();
+			return base.NormalizePath (path);
 		}
 
 		[MonoTODO]
 		public override SourceFileUnit ResolveSourceFileUnit (string name)
 		{
-			throw new NotImplementedException ();
+			return base.ResolveSourceFileUnit (name);
 		}
 
 		[MonoTODO]
 		public override bool SourceDirectoryExists (string path)
 		{
-			throw new NotImplementedException ();
+			return base.SourceDirectoryExists (path);
 		}
 
 		[MonoTODO]
 		public override bool SourceFileExists (string path)
 		{
-			throw new NotImplementedException ();
+			return base.SourceFileExists (path);
 		}
 
 		[MonoTODO]
 		public override bool TryGetVariable (IScriptEngine engine, SymbolId name, out object value)
 		{
-			throw new NotImplementedException ();
+			return base.TryGetVariable (engine, name, out value);
 		}
 
 		[MonoTODO]
 		protected override IList<string> SourceUnitResolutionPath {
-			get { throw new NotImplementedException (); }
+			get { return base.SourceUnitResolutionPath; }
 		}
 	}
 }
