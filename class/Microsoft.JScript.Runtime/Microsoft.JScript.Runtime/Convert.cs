777d4197716f940f70dc1436f5dd32169e7403fe
Marek Sieradzki <msierad@mono-cvs.ximian.com>
2007-06-14 15:33:18 +0000

    2007-06-14  Marek Sieradzki  <marek.sieradzki@gmail.com>
            * JSError.cs
            * Convert.cs: Added.
    	
    
    svn path=/trunk/olive/; revision=79555

---
+++ class/Microsoft.JScript.Runtime/Microsoft.JScript.Runtime/Convert.cs
@@ -0,0 +1,85 @@
+using System;
+using Microsoft.Scripting;
+
+namespace Microsoft.JScript.Runtime {
+	public sealed class Convert {
+		public Convert ()
+		{
+		}
+
+		public static bool CanConvertFrom (Type fromType, Type toType, NarrowingLevel level)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static object Coerce2 (object value, TypeCode target)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static object CoerceT (object value, Type to, bool explicitOk)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static bool ToBoolean (double d)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static bool ToBoolean (object value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static bool ToBoolean (object value, bool explicitConversion)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static int ToInt32 (object value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static double ToNumber (object value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static double ToNumber (string str)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static object ToObject (CodeContext context, object value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static string ToString (bool b)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static string ToString (double d)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static string ToString (object value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static string ToString (object value, bool explicitOk)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static object [] VarArgs (object [] args, int offset, int n)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
