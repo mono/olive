b7483edc79d689fa63633be588aca3f6bbf8eb44
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-06-21 10:57:14 +0000

    * Rect.cs: Add missing members.
    svn path=/trunk/olive/; revision=80430

--- class/agclr/System.Windows/Rect.cs
+++ class/agclr/System.Windows/Rect.cs
@@ -40,6 +40,30 @@ namespace System.Windows {
 			h = height;
 		}
 
+		public Rect(Point location, Size size)
+		{
+			this.x = location.X;
+			this.y = location.Y;
+			this.w = size.Width;
+			this.h = size.Height;
+		}
+		
+		public Rect(Point point1, Point point2)
+		{
+			this.x = point1.X;
+			this.y = point1.Y;
+			this.w = point2.X - point1.X;
+			this.h = point2.Y - point1.Y;
+		}
+		
+		public Rect(Size size)
+		{
+			this.x = 0;
+			this.y = 0;
+			this.w = size.Width;
+			this.h = size.Height;
+		}
+		
 		public override string ToString ()
 		{
 			return String.Format ("{0},{1},{2},{3}", x, y, w, h);
@@ -80,6 +104,206 @@ namespace System.Windows {
 				return false;
 
 			return true;
+		}
+		
+		public bool Contains(double x, double y)
+		{
+			return Contains (new Point (x, y));
+		}
+		
+		public bool Contains(Rect rect)
+		{
+			return Contains (rect.TopLeft) && Contains (rect.BottomRight);
+		}
+		
+		public static Rect Empty { 
+			get { return new Rect (0, 0, 0, 0); } 
+		}
+		
+		public bool IsEmpty { 
+			get {
+				return w <= 0 || h <= 0;
+			}
+		}
+		
+		public Point Location { 
+			get {
+				return new Point (x, y);
+			}
+			set {
+				x = value.X;
+				y = value.Y;
+			}
+		}
+		
+		public Size Size { 
+			get { 
+				return new Size (w, h);
+			}
+			set {
+				w = value.Width;
+				h = value.Height;
+			}
+		}
+		
+		public double Left { 
+			get { return x; }
+		}
+		
+		public double Top { 
+			get { return y; }
+		}
+		
+		public double Right { 
+			get { return x + w; }
+		}
+		
+		public double Bottom { 
+			get { return y + h; }
+		}
+		
+		public Point TopLeft { 
+			get { return new Point (Top, Left); }
+		}
+		
+		public Point TopRight { 
+			get { return new Point (Top, Right); }
+		}
+		
+		public Point BottomLeft { 
+			get { return new Point (Bottom, Left); }
+		}
+		
+		public Point BottomRight { 
+			get { return new Point (Bottom, Right); }
+		}
+		
+		public bool IntersectsWith(Rect rect)
+		{
+			return !((Left >= rect.Right) || (Right <= rect.Left) ||
+			    (Top >= rect.Bottom) || (Bottom <= rect.Top));
+		}
+		
+		public void Intersect(Rect rect)
+		{
+			x = Math.Max (x, rect.x);
+			y = Math.Max (y, rect.y);
+			w = Math.Min (Right, rect.Right) - x;
+			h = Math.Min (Bottom, rect.Bottom) - y; 
+		}
+		
+		public static Rect Intersect(Rect rect1, Rect rect2)
+		{
+			Rect result = rect1;
+			result.Intersect (rect2);
+			return result;
+		}
+		
+		public static Rect Union(Rect rect1, Rect rect2)
+		{
+			Rect result = rect1;
+			result.Union (rect2);
+			return result;
+		}
+		
+		public static Rect Union(Rect rect, Point point)
+		{
+			Rect result = rect;
+			result.Union (point);
+			return result;
+		}
+		
+		public void Union(Rect rect)
+		{
+			x = Math.Min (x, rect.x);
+			y = Math.Min (y, rect.y);
+			w = Math.Max (Right, rect.Right) - x;
+			h = Math.Max (Bottom, rect.Bottom) - y;
+		}
+		
+		public void Union(Point point)
+		{
+			Union (new Rect (point, point));
+		}
+		
+		public void Offset(double offsetX, double offsetY)
+		{
+			x += offsetX;
+			y += offsetY;
+		}
+		
+		public static Rect Offset(Rect rect, double offsetX, double offsetY)
+		{
+			Rect result = rect;
+			result.Offset (offsetX, offsetY);
+			return result;
+		}
+		
+		public void Inflate(Size size)
+		{
+			Inflate (size.Width, size.Height);
+		}
+		
+		public void Inflate(double width, double height)
+		{
+			x -= width;
+			y -= height;
+			w += width;
+			h += height;	
+		}
+		
+		public static Rect Inflate(Rect rect, Size size)
+		{
+			return Inflate (rect, size.Width, size.Height);
+		}
+		
+		public static Rect Inflate(Rect rect, double width, double height)
+		{
+			Rect result = rect;
+			result.Inflate (width, height);
+			return result;
+		}
+		
+		public void Scale(double scaleX, double scaleY)
+		{
+			x *= scaleX;
+			y *= scaleY;
+			w *= scaleX;
+			h *= scaleY;
+		}
+		
+		public static bool operator ==(Rect rect1, Rect rect2)
+		{
+			return rect1.x == rect2.x && rect1.y == rect2.y && rect1.w == rect2.w && rect1.h == rect2.h;
+		}
+		
+		public static bool operator !=(Rect rect1, Rect rect2)
+		{
+			return !(rect1 == rect2);
+		}
+		
+		public static bool Equals(Rect rect1, Rect rect2)
+		{
+			return rect1 == rect2;
+		}
+		
+		public override bool Equals(object o)
+		{
+			if (o is Rect) {
+				return this == (Rect) o;
+			} else {
+				return false;
+			}
+		}
+		
+		public bool Equals(Rect value)
+		{
+			return this == value;
+		}
+		
+		public override int GetHashCode()
+		{
+			return base.GetHashCode ();
 		}
 	}
 }
