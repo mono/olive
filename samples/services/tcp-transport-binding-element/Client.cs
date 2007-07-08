3471f4c63859ce8b88b0343dc0e03839c1f03a43
Marcos Cobena <marcosc@mono-cvs.ximian.com>
2007-07-08 14:38:19 +0000

    New sample for TCP binding element.
    svn path=/trunk/olive/; revision=81596

---
+++ samples/services/tcp-transport-binding-element/Client.cs
@@ -0,0 +1,60 @@
+﻿// 
+// Client.cs
+// 
+// Author: 
+//     Marcos Cobena (marcoscobena@gmail.com)
+// 
+// Copyright 2007 Marcos Cobena (http://www.youcannoteatbits.org/)
+// 
+
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+
+namespace ProgrammingChannels
+{
+	class Client
+	{
+		public static void Main()
+		{
+			RunClient();
+		}
+		
+		static void RunClient()
+		{
+			Console.WriteLine("Press ENTER when service is ready.");
+			Console.ReadLine();
+			
+			CustomBinding binding = new CustomBinding();
+			binding.Elements.Add(new TcpTransportBindingElement());
+			
+			BindingParameterCollection bpcol = 
+				new BindingParameterCollection();
+			
+//			using (IChannelFactory<IDuplexSessionChannel> factory =
+//			binding.BuildChannelFactory<IDuplexSessionChannel>(bpcol))
+//			{
+				IChannelFactory<IDuplexSessionChannel> factory =
+					binding.BuildChannelFactory<IDuplexSessionChannel>(bpcol);
+				
+				factory.Open();
+				
+				IDuplexSessionChannel channel = factory.CreateChannel(
+					new EndpointAddress("net.tcp://localhost/channelapp"));
+				
+				channel.Open();
+				
+				Message message = Message.CreateMessage(
+					//channel.Manager.MessageVersion, 
+					MessageVersion.Default,
+					"some action", "This is the body data");
+				
+				channel.Send(message);
+				
+				channel.Close();
+				factory.Close();
+//			}
+		}
+	}
+}
---
+++ samples/services/tcp-transport-binding-element/Server.cs
@@ -0,0 +1,67 @@
+// 
+// Server.cs
+// 
+// Author: 
+//     Marcos Cobena (marcoscobena@gmail.com)
+// 
+// Copyright 2007 Marcos Cobena (http://www.youcannoteatbits.org/)
+// 
+
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+
+namespace ProgrammingChannels
+{
+	class Service
+	{
+		static void RunService ()
+		{
+			CustomBinding binding = new CustomBinding ();
+			
+			binding.Elements.Add (new TcpTransportBindingElement ());
+			
+			BindingParameterCollection bpcol =
+				new BindingParameterCollection ();
+			
+//			using (IChannelListener<IDuplexSessionChannel> listener =
+//				binding.BuildChannelListener<IDuplexSessionChannel> (
+//					new Uri ("net.tcp://localhost/Server"), bpcol))
+//			{
+				IChannelListener<IDuplexSessionChannel> listener =
+					binding.BuildChannelListener<IDuplexSessionChannel> (
+						new Uri ("net.tcp://localhost/channelapp"), bpcol);
+				
+				listener.Open ();
+				
+				IDuplexSessionChannel channel =
+					listener.AcceptChannel ();
+				
+				Console.WriteLine ("Listening for messages...");
+				
+				channel.Open ();
+				
+				Message message = channel.Receive ();
+				
+				Console.WriteLine ("Message received.");
+				Console.WriteLine ("Message action: {0}.", 
+					message.Headers.Action);
+				Console.WriteLine ("Message content: {0}", 
+					message.GetBody<string> ());
+				
+				message.Close ();
+				channel.Close ();
+				listener.Close ();
+//			}
+		}
+		
+		public static void Main ()
+		{
+			Service.RunService ();
+			
+			Console.WriteLine ("Press ENTER to exit.");
+			Console.ReadLine ();
+		}
+	}
+}
