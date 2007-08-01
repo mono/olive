599fb5cd3950fb993f6dddf37162c07b556427fa
Chris Toshok <toshok@novell.com>
2007-08-01 21:55:40 +0000

    2007-08-01  Chris Toshok  <toshok@ximian.com>
    	* Geometry.cs: Geometry.Transform is a Transform, not a
    	TransformGroup.
    
    
    svn path=/trunk/olive/; revision=83247

--- class/agclr/System.Windows.Media/Geometry.cs
+++ class/agclr/System.Windows.Media/Geometry.cs
@@ -34,7 +34,7 @@ namespace System.Windows.Media {
 		static Geometry ()
 		{
 			FillRuleProperty = DependencyProperty.Lookup (Kind.GEOMETRY, "FillRule", typeof (FillRule));
-			TransformProperty = DependencyProperty.Lookup (Kind.GEOMETRY, "Transform", typeof (TransformGroup));
+			TransformProperty = DependencyProperty.Lookup (Kind.GEOMETRY, "Transform", typeof (Transform));
 		}
 
 		public Geometry () : base (NativeMethods.geometry_new ())
@@ -56,9 +56,9 @@ namespace System.Windows.Media {
 			}
 		}
 
-		public TransformGroup Transform {
+		public Transform Transform {
 			get {
-				return (TransformGroup) GetValue (TransformProperty);
+				return (Transform) GetValue (TransformProperty);
 			}
 			
 			set {
