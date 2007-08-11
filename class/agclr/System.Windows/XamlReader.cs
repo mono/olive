f664cc1742b07cd59684b67d420f42b09bd48cb9
Miguel de Icaza <miguel@gnome.org>
2007-08-11 17:40:18 +0000

    svn path=/trunk/olive/; revision=83879

--- class/agclr/System.Windows/XamlReader.cs
+++ class/agclr/System.Windows/XamlReader.cs
@@ -126,37 +126,6 @@ namespace System.Windows {
 			return p;
 		}
 
-		private static TypeConverter GetConverterFor (PropertyInfo info)
-		{
-			Attribute[] attrs = (Attribute[])info.GetCustomAttributes (true);
-			TypeConverterAttribute at = null;
-			TypeConverter converter = null;
-
-			foreach (Attribute attr in attrs) {
-				if (attr is TypeConverterAttribute) {
-					at = (TypeConverterAttribute)attr;
-					break;
-				}
-			}
-
-			if (at == null || at == TypeConverterAttribute.Default)
-				converter = TypeDescriptor.GetConverter (info.PropertyType);
-			else {
-				Type t = Type.GetType (at.ConverterTypeName);
-				if (t == null) {
-					converter = TypeDescriptor.GetConverter (info.PropertyType);
-				}
-				else {
-					ConstructorInfo ci = t.GetConstructor (new Type[] { typeof(Type) });
-					if (ci != null)
-						converter = (TypeConverter) ci.Invoke (new object[] { info.PropertyType });
-					else
-						converter = (TypeConverter) Activator.CreateInstance (t);
-				}
-			}
-			return converter;
-		}
-		
 		//
 		// Proxy to prevent exceptions from being returned to unmanaged code.
 		//
@@ -178,28 +147,13 @@ namespace System.Windows {
 				return;
 			}
 
-			PropertyInfo pi = target.GetType().GetProperty (name);
-
-			if (pi == null) {
+			string error;
+			Helper.SetPropertyFromString (target, name, value, out error);
+			if (error != null){
 				Console.Error.WriteLine (
-				    "XamlReader, set_attribute: unable to set property ({0}) no property descriptor found", name);
+					"Error: XamlReader, set_attribute on '{0}' from '{1}': {2}", name, value, error);
 				return;
 			}
-
-			TypeConverter converter = GetConverterFor (pi);
-			if (!converter.CanConvertFrom (typeof (string))) {
-				//
-				// MS does not seem to handle this yet either, but I think a logical improvement
-				// here is to call back into unmanaged code something like xaml_create_object_from_str
-				// with the attribute string, and see if the managed code can parse it, this would
-				// allow you to stick things like Colors and KeySplines on your object and still have
-				// custom setters
-				//
-				Console.Error.WriteLine ("XamlReader, set_attribute: unable to convert property '{0}' from a string", name);
-				return;
-			}
-
-			pi.SetValue (target, converter.ConvertFrom (value), null);
 		}
 
 		//
