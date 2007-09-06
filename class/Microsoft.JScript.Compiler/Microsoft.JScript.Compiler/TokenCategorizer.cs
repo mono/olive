25fdbe470cdf697af07af2b0245ee91f4c045b86
Olivier Dufour <olivier@mono-cvs.ximian.com>
2007-09-06 06:13:17 +0000

    2007-09-06  Olivier Dufour  <olivier.Duff@gmail.com>
    	* TokenCategorizer.cs: Implement ReadToken and start to do unit test on it
    
    svn path=/trunk/olive/; revision=85390

--- class/Microsoft.JScript.Compiler/Microsoft.JScript.Compiler/TokenCategorizer.cs
+++ class/Microsoft.JScript.Compiler/Microsoft.JScript.Compiler/TokenCategorizer.cs
@@ -61,140 +61,13 @@ namespace Microsoft.JScript.Compiler
 			return new TokenInfo (span, category, trigger);
 		}
 
-		[MonoTODO]
 		private TokenCategory GetCategory(Token.Type type)
 		{
-			throw new NotImplementedException ();
-			/*switch (type) {				
-				case Token.Type.None:					
-				case Token.Type.EndOfInput:					
-				case Token.Type.LeftBrace:					
-				case Token.Type.RightBrace:					
-				case Token.Type.LeftParenthesis:					
-				case Token.Type.RightParenthesis:					
-				case Token.Type.LeftBracket:					
-				case Token.Type.RightBracket:					
-				case Token.Type.Dot:					
-				case Token.Type.Semicolon:					
-				case Token.Type.Comma:					
-				case Token.Type.Less:					
-				case Token.Type.Greater:					
-				case Token.Type.LessEqual:					
-				case Token.Type.GreaterEqual:					
-				case Token.Type.EqualEqual:					
-				case Token.Type.BangEqual:					
-				case Token.Type.EqualEqualEqual:					
-				case Token.Type.BangEqualEqual:					
-				case Token.Type.Plus:					
-				case Token.Type.Minus:					
-				case Token.Type.Star:					
-				case Token.Type.Percent:					
-				case Token.Type.PlusPlus:					
-				case Token.Type.MinusMinus:					
-				case Token.Type.LessLess:					
-				case Token.Type.GreaterGreater:					
-				case Token.Type.GreaterGreaterGreater:					
-				case Token.Type.Ampersand:					
-				case Token.Type.Bar:					
-				case Token.Type.Circumflex:					
-				case Token.Type.Bang:					
-				case Token.Type.Tilda:					
-				case Token.Type.AmpersandAmpersand:					
-				case Token.Type.BarBar:					
-				case Token.Type.Question:					
-				case Token.Type.Colon:					
-				case Token.Type.Equal:					
-				case Token.Type.PlusEqual:					
-				case Token.Type.MinusEqual:					
-				case Token.Type.StarEqual:					
-				case Token.Type.PercentEqual:					
-				case Token.Type.LessLessEqual:					
-				case Token.Type.GreaterGreaterEqual:					
-				case Token.Type.GreaterGreaterGreaterEqual:					
-				case Token.Type.AmpersandEqual:					
-				case Token.Type.BarEqual:					
-				case Token.Type.CircumflexEqual:					
-				case Token.Type.Divide:					
-				case Token.Type.DivideEqual:
-				case Token.Type.@break:
-				case Token.Type.@else:
-				case Token.Type.@new:					
-				case Token.Type.var:					
-				case Token.Type.@case:					
-				case Token.Type.@finally:					
-				case Token.Type.@return:					
-				case Token.Type.@void:					
-				case Token.Type.@catch:					
-				case Token.Type.@for:					
-				case Token.Type.@switch:					
-				case Token.Type.@while:					
-				case Token.Type.@continue:					
-				case Token.Type.function:					
-				case Token.Type.@this:					
-				case Token.Type.with:					
-				case Token.Type.@default:					
-				case Token.Type.@if:					
-				case Token.Type.@throw:					
-				case Token.Type.delete:					
-				case Token.Type.@in:					
-				case Token.Type.@try:					
-				case Token.Type.@do:					
-				case Token.Type.instanceof:					
-				case Token.Type.@typeof:					
-				case Token.Type.@abstract:					
-				case Token.Type.@enum:					
-				case Token.Type.@int:					
-				case Token.Type.@short:					
-				case Token.Type.boolean:					
-				case Token.Type.export:					
-				case Token.Type.@interface:					
-				case Token.Type.@static:					
-				case Token.Type.@byte:					
-				case Token.Type.extends:					
-				case Token.Type.@long:					
-				case Token.Type.super:					
-				case Token.Type.@char:					
-				case Token.Type.final:					
-				case Token.Type.native:					
-				case Token.Type.synchronized:					
-				case Token.Type.@class:					
-				case Token.Type.@float:					
-				case Token.Type.packate:					
-				case Token.Type.throws:					
-				case Token.Type.@const:					
-				case Token.Type.@goto:					
-				case Token.Type.@private:					
-				case Token.Type.transient:					
-				case Token.Type.debugger:					
-				case Token.Type.implements:					
-				case Token.Type.@protected:					
-				case Token.Type.@volatile:					
-				case Token.Type.@double:					
-				case Token.Type.import:					
-				case Token.Type.@public:					
-				case Token.Type.@null:					
-				case Token.Type.@true:					
-				case Token.Type.@false:					
-				case Token.Type.NumericLiteral:					
-				case Token.Type.HexIntegerLiteral:					
-				case Token.Type.OctalIntegerLiteral:					
-				case Token.Type.StringLiteral:					
-				case Token.Type.RegularExpressionLiteral:					
-				case Token.Type.Identifier:					
-				case Token.Type.Bad:					
-				case Token.Type.Comment:
-					return TokenCategory.Comment;
-			}
-			return TokenCategory.Comment;*/
-		}
-
-		[MonoTODO]
-		private TokenTriggers GetTrigger(Token.Type type)
-		{
-			throw new NotImplementedException ();
-			/*switch (type) {
+			switch (type) {				
 				case Token.Type.None:
+					return TokenCategory.None;
 				case Token.Type.EndOfInput:
+					return TokenCategory.EndOfStream;
 				case Token.Type.LeftBrace:
 				case Token.Type.RightBrace:
 				case Token.Type.LeftParenthesis:
@@ -204,6 +77,145 @@ namespace Microsoft.JScript.Compiler
 				case Token.Type.Dot:
 				case Token.Type.Semicolon:
 				case Token.Type.Comma:
+					return TokenCategory.Delimiter;
+				case Token.Type.Less:
+				case Token.Type.Greater:
+				case Token.Type.LessEqual:
+				case Token.Type.GreaterEqual:
+				case Token.Type.EqualEqual:
+				case Token.Type.BangEqual:
+				case Token.Type.EqualEqualEqual:
+				case Token.Type.BangEqualEqual:
+				case Token.Type.Plus:
+				case Token.Type.Minus:
+				case Token.Type.Star:
+				case Token.Type.Percent:
+				case Token.Type.PlusPlus:
+				case Token.Type.MinusMinus:
+				case Token.Type.LessLess:
+				case Token.Type.GreaterGreater:
+				case Token.Type.GreaterGreaterGreater:
+				case Token.Type.Ampersand:
+				case Token.Type.Bar:
+				case Token.Type.Circumflex:
+				case Token.Type.Bang:
+				case Token.Type.Tilda:
+				case Token.Type.AmpersandAmpersand:
+				case Token.Type.BarBar:
+				case Token.Type.Question:
+				case Token.Type.Colon:
+				case Token.Type.Equal:
+				case Token.Type.PlusEqual:
+				case Token.Type.MinusEqual:
+				case Token.Type.StarEqual:
+				case Token.Type.PercentEqual:
+				case Token.Type.LessLessEqual:
+				case Token.Type.GreaterGreaterEqual:
+				case Token.Type.GreaterGreaterGreaterEqual:
+				case Token.Type.AmpersandEqual:
+				case Token.Type.BarEqual:
+				case Token.Type.CircumflexEqual:
+				case Token.Type.Divide:
+				case Token.Type.DivideEqual:
+					return TokenCategory.Operator;
+				case Token.Type.@break:
+				case Token.Type.@else:
+				case Token.Type.@new:
+				case Token.Type.var:
+				case Token.Type.@case:
+				case Token.Type.@finally:
+				case Token.Type.@return:
+				case Token.Type.@void:
+				case Token.Type.@catch:
+				case Token.Type.@for:
+				case Token.Type.@switch:
+				case Token.Type.@while:
+				case Token.Type.@continue:
+				case Token.Type.function:
+				case Token.Type.@this:
+				case Token.Type.with:
+				case Token.Type.@default:
+				case Token.Type.@if:
+				case Token.Type.@throw:
+				case Token.Type.delete:	
+				case Token.Type.@in:	
+				case Token.Type.@try:	
+				case Token.Type.@do:	
+				case Token.Type.instanceof:	
+				case Token.Type.@typeof:	
+				case Token.Type.@abstract:	
+				case Token.Type.@enum:	
+				case Token.Type.@int:	
+				case Token.Type.@short:	
+				case Token.Type.boolean:	
+				case Token.Type.export:	
+				case Token.Type.@interface:	
+				case Token.Type.@static:	
+				case Token.Type.@byte:	
+				case Token.Type.extends:	
+				case Token.Type.@long:	
+				case Token.Type.super:	
+				case Token.Type.@char:	
+				case Token.Type.final:	
+				case Token.Type.native:	
+				case Token.Type.synchronized:	
+				case Token.Type.@class:	
+				case Token.Type.@float:	
+				case Token.Type.packate:	
+				case Token.Type.throws:	
+				case Token.Type.@const:	
+				case Token.Type.@goto:	
+				case Token.Type.@private:	
+				case Token.Type.transient:	
+				case Token.Type.debugger:	
+				case Token.Type.implements:	
+				case Token.Type.@protected:	
+				case Token.Type.@volatile:	
+				case Token.Type.@double:	
+				case Token.Type.import:	
+				case Token.Type.@public:	
+				case Token.Type.@null:	
+				case Token.Type.@true:	
+				case Token.Type.@false:
+					return TokenCategory.Keyword;
+				case Token.Type.HexIntegerLiteral:
+				case Token.Type.OctalIntegerLiteral:
+				case Token.Type.NumericLiteral:
+					return TokenCategory.NumericLiteral;
+				case Token.Type.StringLiteral:
+					return TokenCategory.StringLiteral;
+				case Token.Type.RegularExpressionLiteral:
+					return TokenCategory.RegularExpressionLiteral;
+				case Token.Type.Identifier:
+					return TokenCategory.Identifier;
+				case Token.Type.Bad:
+					return TokenCategory.Error;
+				case Token.Type.Comment:
+					return TokenCategory.Comment;
+			}
+			return TokenCategory.None;
+		}
+
+		private TokenTriggers GetTrigger(Token.Type type)
+		{
+			switch (type) {
+				case Token.Type.LeftBrace:
+				case Token.Type.RightBrace:
+				case Token.Type.LeftBracket:
+				case Token.Type.RightBracket:
+					return TokenTriggers.MatchBraces;
+				case Token.Type.LeftParenthesis:
+					return TokenTriggers.MatchBraces | TokenTriggers.ParameterStart;
+				case Token.Type.RightParenthesis:
+					return TokenTriggers.MatchBraces | TokenTriggers.ParameterEnd ;
+				case Token.Type.Dot:
+					return TokenTriggers.MemberSelect;
+				case Token.Type.Comma:
+					return TokenTriggers.ParameterNext;
+				//TODO : not found wher e we can use TokenTriggers.Parameter
+				case Token.Type.None:
+				case Token.Type.EndOfInput:
+				case Token.Type.Semicolon:
 				case Token.Type.Less:
 				case Token.Type.Greater:
 				case Token.Type.LessEqual:
@@ -310,9 +322,9 @@ namespace Microsoft.JScript.Compiler
 				case Token.Type.Identifier:
 				case Token.Type.Bad:
 				case Token.Type.Comment:
-					return TokenTriggers.MatchBraces;
+					return TokenTriggers.None;
 			}
-			return TokenTriggers.MatchBraces;*/
+			return TokenTriggers.None;
 		}
 
 		public override IEnumerable<TokenInfo> ReadTokens(int countOfChars)
