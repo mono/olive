27ce94da7f3570186518fefcb87ad2a3d60425c6
Atsushi Eno <atsushieno@gmail.com>
2008-02-08 09:13:36 +0000

    fixed typo.
    svn path=/trunk/olive/; revision=95226

--- samples/services/web-http-binding/samplecli.cs
+++ samples/services/web-http-binding/samplecli.cs
@@ -8,7 +8,7 @@ public class Test
 	// It is not working with mono!
 	public static void Main ()
 	{
-		string url = "http://localhsot:8080";
+		string url = "http://localhost:8080";
 		WebHttpBinding b = new WebHttpBinding ();
 		var f = new WebChannelFactory<IMyService> (b, new Uri (url));
 		// this causes NRE (even if the endpointaddress is specified at CreateChannel()).
