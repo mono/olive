0f3eedbfe6e9a4afe4e83377f77233abf0459f69
Atsushi Eno <atsushieno@gmail.com>
2008-03-22 11:13:24 +0000

    flush
    2008-03-22  Atsushi Enomoto  <atsushi@ximian.com>
    
    	* gettoken.cs : add some dumper.
    
    
    svn path=/trunk/olive/; revision=98789

--- samples/services/cardspace/gettoken.cs
+++ samples/services/cardspace/gettoken.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.IdentityModel.Claims;
 using System.IdentityModel.Selectors;
 using System.IdentityModel.Tokens;
@@ -30,13 +31,16 @@ public class Test
 				.WriteTo (AddressingVersion.WSAddressing10, w);
 		}
 		XmlElement endpoint = doc.DocumentElement.FirstChild as XmlElement;
+		XmlElement issuer = null;
+	if (args.Length > 1) {
 		using (XmlWriter w = doc.DocumentElement.CreateNavigator ().AppendChild ()) {
 			new EndpointAddress (new Uri (args [0]),
 				new X509CertificateEndpointIdentity (new X509Certificate2 (args [1])))
 				.WriteTo (AddressingVersion.WSAddressing10, w);
 
 		}
-		XmlElement issuer = null;//doc.DocumentElement.LastChild as XmlElement;
+		issuer = doc.DocumentElement.LastChild as XmlElement;
+	}
 		string wst = "http://schemas.xmlsoap.org/ws/2005/02/trust";
 		string infocard = "http://schemas.xmlsoap.org/ws/2005/05/identity";
 		XmlElement p = doc.CreateElement ("Claims", wst);
@@ -46,19 +50,29 @@ public class Test
 		ct.SetAttribute ("Uri", ClaimTypes.PPID);
 		p.AppendChild (ct);
 		GenericXmlSecurityToken token = CardSpaceSelector.GetToken (
-			endpoint, new XmlElement [] {p}, issuer, WSSecurityTokenSerializer.DefaultInstance);
+			//endpoint, new XmlElement [] {p}, issuer, WSSecurityTokenSerializer.DefaultInstance);
+			new CardSpacePolicyElement [] {new CardSpacePolicyElement (endpoint, issuer, new Collection<XmlElement> (new XmlElement [] {p}), null, 0, false)}, WSSecurityTokenSerializer.DefaultInstance);
 		XmlWriterSettings s = new XmlWriterSettings ();
 		s.Indent = true;
+		s.ConformanceLevel = ConformanceLevel.Fragment;
 		using (XmlWriter xw = XmlWriter.Create (Console.Out, s)) {
+			// GenericXmlSecurityToken is writable but not readable.
 			WSSecurityTokenSerializer.DefaultInstance.WriteToken (
 				xw, token);
 			// This cannot be serialized.
 			//WSSecurityTokenSerializer.DefaultInstance.WriteToken (
 			//	xw, token.ProofToken);
+			WSSecurityTokenSerializer.DefaultInstance.WriteKeyIdentifierClause (xw, token.InternalTokenReference);
 		}
-		AsymmetricSecurityKey pk = token.ProofToken.SecurityKeys [0]
-			as AsymmetricSecurityKey;
-		Console.WriteLine (pk.HasPrivateKey ());
+		StringWriter sw = new StringWriter ();
+		using (XmlWriter xw = XmlWriter.Create (sw, s)) {
+			WSSecurityTokenSerializer.DefaultInstance.WriteKeyIdentifierClause (xw, token.ExternalTokenReference);
+		}
+		Console.WriteLine (sw);
+		using (XmlReader xr = XmlReader.Create (new StringReader (sw.ToString ()))) {
+			object o = WSSecurityTokenSerializer.DefaultInstance.ReadKeyIdentifierClause (xr);
+Console.WriteLine (o);
+		}
 	}
 }
 
