1a89a0107663e920f4a2ce640757887e9cac4c20
Vladimir Krasnov <krasnov@mono-cvs.ximian.com>
2008-03-23 11:18:44 +0000

    * OperationDescriptionTest.cs: added parts and namespace tests
    svn path=/trunk/olive/; revision=98821

--- class/System.ServiceModel/Test/System.ServiceModel.Description/OperationDescriptionTest.cs
+++ class/System.ServiceModel/Test/System.ServiceModel.Description/OperationDescriptionTest.cs
@@ -67,11 +67,96 @@ namespace MonoTests.System.ServiceModel.Description
 			Assert.IsNull (mb.ReturnValue, "#16"); // void Echo()
 		}
 
+		[Test]
+		public void MessagesNameSpace ()
+		{
+			ContractDescription cd =
+				ContractDescription.GetContract (typeof (IFoo2));
+			OperationDescription od = cd.Operations [0];
+			Assert.IsNull (od.Messages.Find ("Echo"), "#1");
+			MessageDescription md = od.Messages.Find ("http://MonoTests.System.ServiceModel.Description/IFoo2/Echo");
+			Assert.IsNotNull (md, "#2");
+
+			Assert.AreEqual ("http://MonoTests.System.ServiceModel.Description/IFoo2/Echo", md.Action, "#3");
+			Assert.AreEqual (MessageDirection.Input, md.Direction, "#4");
+			Assert.IsFalse (md.HasProtectionLevel, "#5");
+			Assert.IsNotNull (md.Headers, "#6");
+			Assert.AreEqual (0, md.Headers.Count, "#7");
+			Assert.IsNull (md.MessageType, "#8");
+			Assert.IsNotNull (md.Properties, "#9");
+			Assert.AreEqual (0, md.Properties.Count, "#10");
+
+			MessageBodyDescription mb = md.Body;
+			Assert.IsNotNull (mb, "#11");
+			Assert.AreEqual ("Echo", mb.WrapperName, "#12");
+			Assert.AreEqual ("http://MonoTests.System.ServiceModel.Description", mb.WrapperNamespace, "#13");
+			Assert.IsNotNull (mb.Parts, "#14");
+			Assert.AreEqual (0, mb.Parts.Count, "#15");
+			Assert.IsNull (mb.ReturnValue, "#16"); // void Echo()
+		}
+
+		[Test]
+		public void Parts ()
+		{
+			ContractDescription cd =
+				ContractDescription.GetContract (typeof (IFoo3));
+
+			MessagePartDescriptionCollection parts =
+				cd.Operations [0].Messages [0].Body.Parts;
+
+			Assert.AreEqual (1, parts.Count, "#1");
+			MessagePartDescription part = parts [0];
+			Assert.AreEqual ("intValue", part.Name, "#2");
+			Assert.AreEqual ("http://tempuri.org/", part.Namespace, "#3");
+			Assert.AreEqual (typeof (int), part.Type, "#4");
+			Assert.AreEqual (0, part.Index, "#5");
+			Assert.AreEqual (false, part.Multiple, "#5");
+		}
+
+		[Test]
+		public void PartsNamespace ()
+		{
+			ContractDescription cd =
+				ContractDescription.GetContract (typeof (IFoo4));
+
+			MessagePartDescriptionCollection parts =
+				cd.Operations [0].Messages [0].Body.Parts;
+
+			Assert.AreEqual (1, parts.Count, "#1");
+			MessagePartDescription part = parts [0];
+			Assert.AreEqual ("intValue", part.Name, "#2");
+			Assert.AreEqual ("http://MonoTests.System.ServiceModel.Description", part.Namespace, "#3");
+			Assert.AreEqual (typeof (int), part.Type, "#4");
+			Assert.AreEqual (0, part.Index, "#5");
+			Assert.AreEqual (false, part.Multiple, "#5");
+		}
+		
 		[ServiceContract]
 		public interface IFoo
 		{
 			[OperationContract]
 			void Echo ();
+		}
+		
+		[ServiceContract(Namespace="http://MonoTests.System.ServiceModel.Description")]
+		public interface IFoo2
+		{
+			[OperationContract]
+			void Echo ();
+		}
+
+		[ServiceContract]
+		public interface IFoo3
+		{
+			[OperationContract]
+			int Echo (int intValue);
+		}
+
+		[ServiceContract (Namespace = "http://MonoTests.System.ServiceModel.Description")]
+		public interface IFoo4
+		{
+			[OperationContract]
+			int Echo (int intValue);
 		}
 	}
 }
