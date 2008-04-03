79d60ecec55f300d9949d6c0d09d8262df2b3dd1
Roei Erez <roeie@mono-cvs.ximian.com>
2008-04-03 12:19:42 +0000

    Add client server messaging tests.
    svn path=/trunk/olive/; revision=99740

---
+++ class/System.ServiceModel/Test/FeatureBased/Features.Serialization/DataContractSerializerTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using MonoTests.Features;
+using MonoTests.Features.Contracts;
+using NUnit.Framework;
+namespace MonoTests.Features.Serialization
+{
+	[TestFixture]
+	[Category ("NotWorking")]
+	public class DataContractSerializerTest : TestFixtureBase<DataContractTesterContractClient,  DataContractTester>
+	{		
+
+		[Test]
+		public void TestPrimitiveComplexType () {
+			monotests.features.client.ComplexPrimitiveClass n1 = GetNewDataInstance ();
+			monotests.features.client.ComplexPrimitiveClass n2 = GetNewDataInstance ();
+			monotests.features.client.ComplexPrimitiveClass result = Client.Add (n1, n2);
+			Assert.IsTrue (result.byteMember == 2);
+			Assert.IsTrue (result.sbyteMember == 2);
+			Assert.IsTrue (result.shortMember == 2);
+			Assert.IsTrue (result.ushortMember == 2);
+			Assert.IsTrue (result.intMember == 2);
+			Assert.IsTrue (result.uintMember == 2);
+			Assert.IsTrue (result.longMember == 2);
+			Assert.IsTrue (result.ulongMember == 2);
+			Assert.IsTrue (result.doubleMember == 2);
+			Assert.IsTrue (result.floatMember == 2);
+		}
+
+		[Test]
+		public void TestPrimitiveComplexTypeByRef () {
+			monotests.features.client.ComplexPrimitiveClass n1 = GetNewDataInstance ();
+			monotests.features.client.ComplexPrimitiveClass n2 = GetNewDataInstance ();
+			monotests.features.client.ComplexPrimitiveClass result = null;
+			result = Client.AddByRef (n1, n2);
+			Assert.IsTrue (result.byteMember == 2);
+			Assert.IsTrue (result.sbyteMember == 2);
+			Assert.IsTrue (result.shortMember == 2);
+			Assert.IsTrue (result.ushortMember == 2);
+			Assert.IsTrue (result.intMember == 2);
+			Assert.IsTrue (result.uintMember == 2);
+			Assert.IsTrue (result.longMember == 2);
+			Assert.IsTrue (result.ulongMember == 2);
+			Assert.IsTrue (result.doubleMember == 2);
+			Assert.IsTrue (result.floatMember == 2);
+		}
+
+		private monotests.features.client.ComplexPrimitiveClass GetNewDataInstance () {
+			monotests.features.client.ComplexPrimitiveClass inst = new monotests.features.client.ComplexPrimitiveClass ();
+			inst.byteMember = 1;
+			inst.sbyteMember = 1;
+			inst.intMember = 1;
+			inst.uintMember = 1;
+			inst.shortMember = 1;
+			inst.ushortMember = 1;
+			inst.longMember = 1;
+			inst.ulongMember = 1;
+			inst.doubleMember = 1;
+			inst.floatMember = 1;
+			return inst;
+		}
+	}
+}
---
+++ class/System.ServiceModel/Test/FeatureBased/Features.Serialization/PrimitiveTesterTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using MonoTests.Features;
+using MonoTests.Features.Contracts;
+using NUnit.Framework;
+
+namespace MonoTests.Features.Serialization
+{
+	[TestFixture]
+	[Category("NotWorking")]
+	public class PrimitiveTesterTest : TestFixtureBase<PrimitiveTesterContractClient, PrimitiveTester>
+	{
+		[Test]
+		public void TestDouble () {
+			Assert.IsTrue (Client.AddDouble (1, 1) == 2);
+		}
+
+		[Test]
+		public void TestByte () {
+			Assert.IsTrue (Client.AddByte (1, 1) == 2);
+		}
+
+		[Test]
+		public void TestSByte () {
+			Assert.IsTrue (Client.AddSByte (1, 1) == 2);
+		}
+
+		[Test]
+		public void TestShort () {
+			Assert.IsTrue (Client.AddShort (1, 1) == 2);
+		}
+
+		[Test]
+		public void TestUShort () {
+			Assert.IsTrue (Client.AddUShort (1, 1) == 2);
+		}
+
+		[Test]
+		public void TestInt () {
+			Assert.IsTrue (Client.AddInt (1, 1) == 2);
+		}
+
+		[Test]
+		public void TestUInt () {
+			Assert.IsTrue (Client.AddUInt (1, 1) == 2);
+		}
+
+		[Test]
+		public void TestLong () {
+			Assert.IsTrue (Client.AddLong (1, 1) == 2);
+		}
+
+		[Test]
+		public void TestULong () {
+			Assert.IsTrue (Client.AddULong (1, 1) == 2);
+		}
+
+		[Test]
+		public void TestFloat () {
+			Assert.IsTrue (Client.AddFloat (1, 1) == 2);
+		}
+
+		[Test]
+		public void TestByRef () {
+			double d;
+			double res = Client.AddByRef (out d, 1, 1);
+			Assert.IsTrue(d == res);
+		}
+	}
+}
