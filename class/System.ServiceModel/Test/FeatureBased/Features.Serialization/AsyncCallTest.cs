a9972ee6020153df825745c0d52b50021576fe30
Vladimir Krasnov <krasnov@mono-cvs.ximian.com>
2008-04-07 16:43:50 +0000

    * added: MessageContractTesterProxy.cs, AsyncCallTesterProxy.cs, FaultsTesterProxy.cs, AsyncCallTester.cs, FaultsTester.cs, FaultsTest.cs, MessageContractTest.cs, AsyncCallTest.cs * fixed: MessageContractTester.cs * fixed: UntypedMessageTesterProxy.cs
    svn path=/trunk/olive/; revision=100034

---
+++ class/System.ServiceModel/Test/FeatureBased/Features.Serialization/AsyncCallTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Proxy.MonoTests.Features.Client;
+
+namespace MonoTests.Features.Serialization
+{
+	[TestFixture]
+	[Category ("NotWorking")]
+	public class AsyncCallTest : TestFixtureBase<AsyncCallTesterContractClient, MonoTests.Features.Contracts.AsyncCallTester>
+	{
+		bool client_QueryCompleted;
+		string s = string.Empty;
+
+		[Test]
+		public void TestAsyncCall ()
+		{
+			client_QueryCompleted = false;
+
+			Client.QueryCompleted += new EventHandler<QueryCompletedEventArgs>(Client_QueryCompleted);
+			Client.QueryAsync ("heh");
+		}
+
+		private void Client_QueryCompleted (object sender, QueryCompletedEventArgs e)
+		{
+			client_QueryCompleted = true;
+			s = e.Result;
+			Assert.AreEqual ("hehheh", s, "#1");
+		}
+	}
+}
