c6f26b015c57ea885bf410b392b99f9a676107da
Olivier Dufour <olivier@mono-cvs.ximian.com>
2008-10-14 05:05:17 +0000

    2008-10-14  Olivier Dufour  <olivier.duff@gmail.com>
    	* Rename namespace and class
    
    svn path=/trunk/olive/; revision=115739

--- class/Microsoft.JScript.Runtime/Microsoft.JScript.Runtime.Conversions/DictionaryGenericWrapper.cs
+++ class/Microsoft.JScript.Runtime/Microsoft.JScript.Runtime.Conversions/DictionaryGenericWrapper.cs
@@ -1,4 +1,4 @@
-// ConcatString.cs
+// DictionaryGenericWrapper.cs
 //
 // Authors:
 //   Olivier Dufour <olivier.duff@gmail.com>
@@ -30,7 +30,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Microsoft.JScript.Runtime
+namespace Microsoft.JScript.Runtime.Conversions
 {
     
     
--- class/Microsoft.JScript.Runtime/Microsoft.JScript.Runtime.Conversions/ListGenericWrapper.cs
+++ class/Microsoft.JScript.Runtime/Microsoft.JScript.Runtime.Conversions/ListGenericWrapper.cs
@@ -30,7 +30,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Microsoft.JScript.Runtime
+namespace Microsoft.JScript.Runtime.Conversions
 {
     
     
