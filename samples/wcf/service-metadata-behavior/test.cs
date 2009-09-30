61889dfb5611d312d01749ebd20e05c602b45642
Atsushi Eno <atsushieno@gmail.com>
2009-09-30 16:00:50 +0000

    Update samples that should rather match .NET behavior. Now web.config is involved. It somehow fails now, so I have added non-xsp case (standalone Http ServiceHost) too.
    svn path=/trunk/olive/; revision=143033

--- samples/wcf/service-metadata-behavior/test.cs
+++ samples/wcf/service-metadata-behavior/test.cs
@@ -2,6 +2,20 @@ using System;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 
+#if CONSOLE
+public class Test
+{
+	public static void Main ()
+	{
+		var host = new ServiceHost (typeof (TestService), new Uri ("http://localhost:8080/test.svc"));
+		host.Open ();
+		Console.WriteLine ("Type [CR] to quit...");
+		Console.ReadLine ();
+		host.Close ();
+	}
+}
+#endif
+
 [ServiceContract]
 public interface ITestService
 {
@@ -13,6 +27,7 @@ public class TestService : ITestService
 {
 	public string Echo (string msg)
 	{
+Console.WriteLine (OperationContext.Current.Host.ChannelDispatchers.Count);
 		return msg;
 	}
 }
