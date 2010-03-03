4601a2eb4266767821362922b45683464edfdaa9
Atsushi Eno <atsushieno@gmail.com>
2010-03-03 15:24:29 +0000

    2010-03-03  Atsushi Enomoto  <atsushi@ximian.com>
            * Makefile, samplesvc.cs, samplecli.cs : initial checkin.
    
    svn path=/trunk/olive/; revision=152940

---
+++ samples/wcf/authorization/samplecli.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Xml;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+public class Tset
+{
+	public static void Main ()
+	{
+		Run ("user", "right");
+		Run ("user", "wrong");
+	}
+
+	static void Run (string user, string pass)
+	{
+		var binding = new BasicHttpBinding ();
+		binding.Security.Mode =
+			BasicHttpSecurityMode.TransportCredentialOnly;
+		binding.Security.Transport.ClientCredentialType =
+			HttpClientCredentialType.Basic;
+		FooProxy proxy = new FooProxy (binding,
+			new EndpointAddress ("http://localhost:8080/"));
+		proxy.ClientCredentials.UserName.UserName = user;
+		proxy.ClientCredentials.UserName.Password = pass;
+		proxy.Open ();
+		Console.WriteLine (proxy.Echo ("TEST FOR ECHO"));
+		Console.WriteLine (proxy.Add (1000, 2000));
+		Console.WriteLine ("done");
+	}
+}
+
+[ServiceContract]
+public interface IFoo
+{
+	[OperationContract]
+	string Echo (string msg);
+
+	[OperationContract]
+	uint Add (uint v1, uint v2);
+}
+
+public class FooProxy : ClientBase<IFoo>, IFoo
+{
+	public FooProxy (Binding binding, EndpointAddress address)
+		: base (binding, address)
+	{
+	}
+
+	public string Echo (string msg)
+	{
+		return Channel.Echo (msg);
+	}
+
+	public uint Add (uint v1, uint v2)
+	{
+		return Channel.Add (v1, v2);
+	}
+}
+
---
+++ samples/wcf/authorization/samplesvc.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IdentityModel.Selectors;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Security;
+
+public class Tset
+{
+	public static void Main ()
+	{
+		ServiceHost host = new ServiceHost (typeof (Foo));
+		var sc = new ServiceCredentials ();
+		sc.UserNameAuthentication.UserNamePasswordValidationMode =
+			UserNamePasswordValidationMode.Custom;
+		sc.UserNameAuthentication.CustomUserNamePasswordValidator =
+			new TestUserNamePasswordValidator ();
+		host.Description.Behaviors.Add (sc);
+		var binding = new BasicHttpBinding ();
+		binding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
+		binding.Security.Transport.ClientCredentialType =
+			HttpClientCredentialType.Basic;
+		binding.ReceiveTimeout = TimeSpan.FromSeconds (5);
+		binding.OpenTimeout = TimeSpan.FromSeconds (20);
+		host.AddServiceEndpoint ("IFoo",
+			binding, new Uri ("http://localhost:8080"));
+		host.Description.Behaviors.Find<ServiceBehaviorAttribute> ()
+			.ConcurrencyMode = ConcurrencyMode.Multiple;
+		host.Description.Behaviors.Add (new ServiceMetadataBehavior () { HttpGetUrl = new Uri ("http://localhost:8080/wsdl"), HttpGetEnabled = true });
+		host.Open ();
+		Console.WriteLine ("Hit [CR] key to close ...");
+		Console.ReadLine ();
+		host.Close ();
+		((IDisposable) host).Dispose ();
+	}
+}
+
+[ServiceContract]
+public interface IFoo
+{
+	[OperationContract]
+	string Echo (string msg);
+
+	[OperationContract]
+	uint Add (uint v1, uint v2);
+}
+
+class Foo : IFoo
+{
+	public string Echo (string msg) 
+	{
+Console.WriteLine (OperationContext.Current.IncomingMessageHeaders.Action);
+Console.WriteLine (OperationContext.Current.Channel.GetType ());
+		return msg + msg;
+		//throw new NotImplementedException ();
+	}
+
+	public uint Add (uint v1, uint v2)
+	{
+Console.WriteLine ("ADD");
+Console.WriteLine (OperationContext.Current.Channel.GetType ());
+		return v1 + v2;
+	}
+}
+
+
+public class TestUserNamePasswordValidator : UserNamePasswordValidator
+{
+	public override void Validate (string user, string pass)
+	{
+		Console.WriteLine ("User {0} / Pass {1}", user, pass);
+		if (user != "user" || pass != "right")
+			throw new Exception (String.Format ("Authentication failed for {0} and {1}", user, pass));
+	}
+}
