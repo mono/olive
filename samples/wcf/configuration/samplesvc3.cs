450c5a119f646e2ac76bc15806daea6ab1ca233d
Atsushi Eno <atsushieno@gmail.com>
2010-03-16 07:08:37 +0000

    New set of test sources. sample3 is sample2 based on asp.net.
    svn path=/trunk/olive/; revision=153631

---
+++ samples/wcf/configuration/samplesvc3.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Security;
+
+[ServiceContract]
+public interface IFoo
+{
+	[OperationContract]
+	string Echo (string msg);
+
+	[OperationContract]
+	uint Add (uint v1, uint v2);
+}
+
+class Foo : IFoo
+{
+	public string Echo (string msg) 
+	{
+Console.WriteLine ("ECHO");
+		return msg + msg;
+	}
+
+	public uint Add (uint v1, uint v2)
+	{
+Console.WriteLine ("ADD");
+		return v1 + v2;
+	}
+}
