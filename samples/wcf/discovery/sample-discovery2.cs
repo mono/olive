8eef4a45fdc651cbbe60a7a59ad2089dad0e3e0b
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2010-09-03 01:50:58 +0900

    Add another sample: Announcement is done via UDP, discovery is HTTP.

---
+++ samples/wcf/discovery/sample-discovery2.cs
@@ -0,0 +1,55 @@
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Discovery;
+
+public class Test
+{
+	public static void Main ()
+	{
+		RunCodeUnderDiscoveryHost (new Uri ("http://localhost:37564"), new Uri ("http://localhost:4949"));
+	}
+
+	static void RunCodeUnderDiscoveryHost (Uri serviceUri, Uri dHostUri)
+	{
+		// announcement service
+		var aEndpoint = new UdpAnnouncementEndpoint (new Uri ("soap.udp://239.255.255.250:3802/"));
+		
+		// discovery service
+		var dbinding = new CustomBinding (new HttpTransportBindingElement ());
+		var dAddress = new EndpointAddress (dHostUri);
+		var dEndpoint = new DiscoveryEndpoint (dbinding, dAddress);
+		// Without this, .NET rejects the host as if it had no service.
+		dEndpoint.IsSystemEndpoint = false;
+		var ib = new InspectionBehavior ();
+		ib.RequestReceived += delegate (ref Message msg, IClientChannel channel, InstanceContext instanceContext) {
+			var mb = msg.CreateBufferedCopy (0x10000);
+			msg = mb.CreateMessage ();
+			Console.Error.WriteLine (mb.CreateMessage ());
+			return null;
+			};
+		ib.ReplySending += delegate (ref Message msg, object o) {
+			var mb = msg.CreateBufferedCopy (0x10000);
+			msg = mb.CreateMessage ();
+			Console.Error.WriteLine (mb.CreateMessage ());
+			};
+
+		dEndpoint.Behaviors.Add (ib);
+		aEndpoint.Behaviors.Add (ib);
+
+		// it internally hosts an AnnouncementService
+		using (var inst = new AnnouncementBoundDiscoveryService (aEndpoint)) {
+			var host = new ServiceHost (inst);
+			host.AddServiceEndpoint (dEndpoint);
+			host.Description.Behaviors.Find<ServiceDebugBehavior> ()
+				.IncludeExceptionDetailInFaults = true;
+			host.Open ();
+			Console.WriteLine ("Type [CR] to quit...");
+			Console.ReadLine ();
+			host.Close ();
+		}
+	}
+}
+
+
---
+++ samples/wcf/discovery/sample-service2.cs
@@ -0,0 +1,54 @@
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Discovery;
+
+public class Test
+{
+	public static void Main ()
+	{
+		RunCodeUnderDiscoveryHost (new Uri ("http://localhost:37564"), new Uri ("http://localhost:4949"), UseCase1Core);
+	}
+
+	static void RunCodeUnderDiscoveryHost (Uri serviceUri, Uri dHostUri, Action<Uri,AnnouncementEndpoint,DiscoveryEndpoint> action)
+	{
+		var aEndpoint = new UdpAnnouncementEndpoint (new Uri ("soap.udp://239.255.255.250:3802/"));
+		var dbinding = new CustomBinding (new HttpTransportBindingElement ());
+		var dAddress = new EndpointAddress (dHostUri);
+		var dEndpoint = new DiscoveryEndpoint (dbinding, dAddress);
+
+		var ib = new InspectionBehavior ();
+		ib.RequestReceived += delegate (ref Message msg, IClientChannel
+channel, InstanceContext instanceContext) {
+			var mb = msg.CreateBufferedCopy (0x10000);
+			msg = mb.CreateMessage ();
+			Console.Error.WriteLine (mb.CreateMessage ());
+			return null;
+			};
+		ib.ReplySending += delegate (ref Message msg, object o) {
+			var mb = msg.CreateBufferedCopy (0x10000);
+			msg = mb.CreateMessage ();
+			Console.Error.WriteLine (mb.CreateMessage ());
+			};
+
+		dEndpoint.Behaviors.Add (ib);
+		aEndpoint.Behaviors.Add (ib);
+
+		action (serviceUri, aEndpoint, dEndpoint);
+	}
+
+	static void UseCase1Core (Uri serviceUri, AnnouncementEndpoint aEndpoint, DiscoveryEndpoint dEndpoint)
+	{
+		// actual service, announcing to 4989
+		var host = new ServiceHost (typeof (TestService));
+		var sdb = new ServiceDiscoveryBehavior ();
+		sdb.AnnouncementEndpoints.Add (aEndpoint);
+		host.Description.Behaviors.Add (sdb);
+		host.AddServiceEndpoint (typeof (ITestService), new BasicHttpBinding (), serviceUri);
+		host.Open ();
+		Console.WriteLine ("Type [CR] to quit ...");
+		Console.ReadLine ();
+		host.Close ();
+	}
+}
+
