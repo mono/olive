6ba52962bd0d05c12c194438d71647f686d6faac
Antonius Riha <antoniusriha@gmail.com>
2014-01-06 18:12:49 +0100

    Add CollectionViewGroup stub

---
+++ class/PresentationFramework/System.Windows.Data/CollectionViewGroup.cs
@@ -0,0 +1,82 @@
+//
+// CollectionViewGroup.cs
+//
+// Author:
+//       Antonius Riha <antoniusriha@gmail.com>
+//
+// Copyright (c) 2012 Antonius Riha
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace System.Windows.Data
+{
+	public abstract class CollectionViewGroup : INotifyPropertyChanged
+	{
+		readonly object name;
+
+		protected CollectionViewGroup (object name)
+		{
+			if (name == null)
+				throw new ArgumentNullException ("name");
+			this.name = name;
+		}
+
+		public abstract bool IsBottomLevel { get; }
+
+		public int ItemCount {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public ReadOnlyObservableCollection<object> Items {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public object Name {
+			get { return name; }
+		}
+
+		protected int ProtectedItemCount { get; set; }
+
+		protected ObservableCollection<object> ProtectedItems {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		protected virtual void OnPropertyChanged (PropertyChangedEventArgs e)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged (this, e);
+		}
+
+		protected virtual event PropertyChangedEventHandler PropertyChanged;
+
+		event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged {
+			add { PropertyChanged += value; }
+			remove { PropertyChanged -= value; }
+		}
+	}
+}
